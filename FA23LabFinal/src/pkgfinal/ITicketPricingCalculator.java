a8f8016f8223ae52735da5d3b498d08e8f16d31b
Mubashir Ahmed <83922497+mubashirahmed07@users.noreply.github.com>
2023-12-29 23:21:14 +0500

    Add files via upload
    Question:
    The Baltimore Orioles Stadium Ticket Office invites software firms to bid for a project. They
    want to have a class library (with classes and interface) to calculate ticket sale price. Here are the
    rules for ticket sale at Stadium:
    • Ticket sale price consists of two parts: ticket price and sale tax.
    • Ticket price varies between children (age under 16 - $10), adults ($15) and seniors (age
    65+ - $12).
    The Baltimore Orioles Stadium Ticket Office will award the project to the design that allows
    users to add more ticket categories and tax rate calculation changes easily.

---
+++ FA23LabFinal/src/pkgfinal/ITicketPricingCalculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author MUBASHIR AHMED
+ */
+class ITicketPricingCalculator implements ITicketPricingStrategy {
+    public int CalculateTicketPrice(int age) {
+        if (age < 16) {
+            return 10;
+        } else if (age >= 65) {
+            return 12;
+        } else {
+            return 15;
+        }
+    }
+}
---
+++ FA23LabFinal/src/pkgfinal/SaleTaxCalculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author MUBASHIR AHMED
+ */
+class SaleTaxCalculator {
+    public int CalculateSaleTax(int ticketPrice, int taxRate) {
+        return ticketPrice * taxRate;
+    }
+}
